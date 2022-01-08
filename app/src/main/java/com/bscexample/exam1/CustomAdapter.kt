8b5002faaeb5d52f6127d5ae4f078c69b22db6f6
bea0266 <bea0266@naver.com>
2022-01-08 18:40:17 +0900

    recyclerview 변경 및 뷰홀더 생성

--- app/src/main/java/com/bscexample/exam1/CustomAdapter.kt
+++ app/src/main/java/com/bscexample/exam1/CustomAdapter.kt
@@ -1,47 +1,54 @@
 package com.bscexample.exam1
 
 import android.content.Context
-import android.text.Layout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
 import android.widget.TextView
-import com.bscexample.exam1.databinding.HeroListItemBinding
+import androidx.recyclerview.widget.RecyclerView
+import com.bscexample.exam1.databinding.HeroRecyclerItemBinding
 
 
-class CustomAdapter(val context: Context, var data : ArrayList<Hero>) : BaseAdapter() {
+class CustomAdapter : RecyclerView.Adapter<CustomAdapter.Holder>() {
 
 
-    override fun getCount(): Int {
+    var data = mutableListOf<Hero>()
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val binding = HeroRecyclerItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return Holder(binding)
+    }
+
+    override fun getItemCount(): Int {
         return data.size
     }
 
-    override fun getItem(position: Int): Any {
-
-        return data.get(position)
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        val hero = data.get(position)
+        holder.setHero(hero)
     }
 
-    override fun getItemId(position: Int): Long {
-
-        return position.toLong()
-    }
-
-    override fun getView(position: Int, convertview: View?, parent: ViewGroup?): View {
-
-        val view: View = LayoutInflater.from(context).inflate(R.layout.hero_list_item, null)
-        val tvName = view.findViewById<TextView>(R.id.tvName)
-        val tvPos = view.findViewById<TextView>(R.id.tvPos)
-        val tvLine = view.findViewById<TextView>(R.id.tvLine)
-        val item: Hero = data[position]
-        tvName.text = item.name
-        tvPos.text = item.pos
-        tvLine.text = item.line
-
-        return view
-
+    fun loadData(name: String, pos: String, line: String) {
+        val hero = Hero(name, pos, line)
+        data.add(hero)
 
 
     }
+
+    class Holder(val binding: HeroRecyclerItemBinding): RecyclerView.ViewHolder(binding.root) {
+
+        fun setHero(hero: Hero)
+        {
+            binding.tvName.text= hero.name
+            binding.tvPos.text=hero.pos
+            binding.tvLine.text=hero.line
+        }
+
+    }
+
+
+
+
+
+
 }
